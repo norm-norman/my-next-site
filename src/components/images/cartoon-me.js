177f356f595ff423cf8360dbf1c08449fccaaa59
Norm Norman <34076664+norm-norman@users.noreply.github.com>
2024-03-14 21:04:58 -0400

    Update cartoon-me.js

--- src/components/images/cartoon-me.js
+++ src/components/images/cartoon-me.js
@@ -7,27 +7,6 @@ import Image from 'next/image';
 const CartoonMe = ({ priority, id, url, size, isSmallestScreen }) => {
   return (
     <Box id={`${id}Wrapper`} alignItems={'center'} justifyContent={'center'}>
-<<<<<<< Updated upstream
-      <Tooltip
-        disableFocusListener
-        followCursor
-        arrow
-        TransitionComponent={Fade}
-        TransitionProps={{ timeout: 400 }}
-        title={<p>I was made by the GenAI Text-To-Image tool on Canva!</p>}
-      >
-        {!isSmallestScreen && (
-          <Image
-            priority={priority}
-            id={id}
-            src={url}
-            alt="Cartoon picture of myself"
-            width={size}
-            height={size}
-          />
-        )}
-      </Tooltip>
-=======
       {!isSmallestScreen && (
         <Image
           priority={priority}
@@ -38,7 +17,6 @@ const CartoonMe = ({ priority, id, url, size, isSmallestScreen }) => {
           height={size * 0.9}
         />
       )}
->>>>>>> Stashed changes
     </Box>
   );
 };
